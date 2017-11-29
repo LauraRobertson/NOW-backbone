1c7cae43cb5933fd8a2e9cdb56cb2646a9665c86
Laura Robertson <mslaura.mae@gmail.com>
2017-11-28 17:30:55 -0800

    Song list added, songs append #song-list

--- src/app.js
+++ src/app.js
@@ -6,8 +6,44 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-console.log('it loaded!');
+import Song from './models/song';
+import SongList from './collections/song_list';
+
+// console.log('it loaded!');
+
+const songData = [
+    {
+        title: "I'm too sexy",
+        year: 1991,
+        artist: "Right Said Fred"
+    }, {
+        title: "Macarena",
+        year: 1993,
+        artist: "Los Del Rio"
+    }, {
+        title: "Tainted Love",
+        year: 1982,
+        artist: "Soft Cell"
+    }, {
+        title: "Take on Me",
+        year: 1985,
+        artist: "A-ha"
+    }
+  ];
+
+const songList = new SongList(songData);
+
+let songTemplate;
+
+const render = function render(songList) {
+  const $songList = $('#song-list');
+
+  songList.forEach((song) => {
+    $songList.append(songTemplate(song.attributes));
+  });
+};
 
 $(document).ready( () => {
-  
+  songTemplate = _.template($('#song-template').html());
+  render(songList);
 });
